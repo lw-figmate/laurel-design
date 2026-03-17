7888fb7d1ff7076def0981fa261d7a2034711b18
Lawrence Wiggins <lwiggins@figma.com>
2026-03-17 14:24:50 -0400

    feat: add new icons and connect to Figma

---
+++ src/atoms/Icon/icons/icons.figma.tsx
@@ -0,0 +1,328 @@
+import figma from '@figma/code-connect';
+import { AlertCircleIcon } from './AlertCircleIcon';
+import { ArrowDownIcon } from './ArrowDownIcon';
+import { ArrowLeftIcon } from './ArrowLeftIcon';
+import { ArrowRightIcon } from './ArrowRightIcon';
+import { ArrowUpIcon } from './ArrowUpIcon';
+import { BanIcon } from './BanIcon';
+import { BellIcon } from './BellIcon';
+import { BookmarkIcon } from './BookmarkIcon';
+import { CalendarIcon } from './CalendarIcon';
+import { CameraIcon } from './CameraIcon';
+import { CheckIcon } from './CheckIcon';
+import { CheckCircleIcon } from './CheckCircleIcon';
+import { ChevronDownIcon } from './ChevronDownIcon';
+import { ChevronLeftIcon } from './ChevronLeftIcon';
+import { ChevronRightIcon } from './ChevronRightIcon';
+import { ChevronUpIcon } from './ChevronUpIcon';
+import { ClipboardIcon } from './ClipboardIcon';
+import { ClockIcon } from './ClockIcon';
+import { CodeIcon } from './CodeIcon';
+import { CopyIcon } from './CopyIcon';
+import { DatabaseIcon } from './DatabaseIcon';
+import { DownloadIcon } from './DownloadIcon';
+import { EditIcon } from './EditIcon';
+import { ExternalLinkIcon } from './ExternalLinkIcon';
+import { EyeIcon } from './EyeIcon';
+import { EyeOffIcon } from './EyeOffIcon';
+import { FilterIcon } from './FilterIcon';
+import { GlobeIcon } from './GlobeIcon';
+import { HeartIcon } from './HeartIcon';
+import { HelpCircleIcon } from './HelpCircleIcon';
+import { HomeIcon } from './HomeIcon';
+import { ImageIcon } from './ImageIcon';
+import { InfoIcon } from './InfoIcon';
+import { LayersIcon } from './LayersIcon';
+import { LinkIcon } from './LinkIcon';
+import { LockIcon } from './LockIcon';
+import { MailIcon } from './MailIcon';
+import { MapPinIcon } from './MapPinIcon';
+import { MenuIcon } from './MenuIcon';
+import { MinusIcon } from './MinusIcon';
+import { MoreHorizontalIcon } from './MoreHorizontalIcon';
+import { MoreVerticalIcon } from './MoreVerticalIcon';
+import { MusicIcon } from './MusicIcon';
+import { PauseIcon } from './PauseIcon';
+import { PlayIcon } from './PlayIcon';
+import { PlusIcon } from './PlusIcon';
+import { RefreshIcon } from './RefreshIcon';
+import { SaveIcon } from './SaveIcon';
+import { SearchIcon } from './SearchIcon';
+import { SettingsIcon } from './SettingsIcon';
+import { ShareIcon } from './ShareIcon';
+import { SortIcon } from './SortIcon';
+import { StarIcon } from './StarIcon';
+import { TerminalIcon } from './TerminalIcon';
+import { ThumbsUpIcon } from './ThumbsUpIcon';
+import { TrashIcon } from './TrashIcon';
+import { UploadIcon } from './UploadIcon';
+import { UserIcon } from './UserIcon';
+import { UsersIcon } from './UsersIcon';
+import { WarningIcon } from './WarningIcon';
+import { XIcon } from './XIcon';
+import { XCircleIcon } from './XCircleIcon';
+import { ZapIcon } from './ZapIcon';
+
+// ── Navigation ───────────────────────────────────────────────────────
+
+figma.connect(ArrowUpIcon, 'https://www.figma.com/design/Ni2hCq5zflPlamYJfpIV68/Laurelma-DS?node-id=14:2651', {
+  example: () => <ArrowUpIcon />,
+});
+
+figma.connect(ArrowDownIcon, 'https://www.figma.com/design/Ni2hCq5zflPlamYJfpIV68/Laurelma-DS?node-id=14:2655', {
+  example: () => <ArrowDownIcon />,
+});
+
+figma.connect(ArrowLeftIcon, 'https://www.figma.com/design/Ni2hCq5zflPlamYJfpIV68/Laurelma-DS?node-id=14:2659', {
+  example: () => <ArrowLeftIcon />,
+});
+
+figma.connect(ArrowRightIcon, 'https://www.figma.com/design/Ni2hCq5zflPlamYJfpIV68/Laurelma-DS?node-id=14:2663', {
+  example: () => <ArrowRightIcon />,
+});
+
+figma.connect(ChevronUpIcon, 'https://www.figma.com/design/Ni2hCq5zflPlamYJfpIV68/Laurelma-DS?node-id=14:2667', {
+  example: () => <ChevronUpIcon />,
+});
+
+figma.connect(ChevronDownIcon, 'https://www.figma.com/design/Ni2hCq5zflPlamYJfpIV68/Laurelma-DS?node-id=14:2670', {
+  example: () => <ChevronDownIcon />,
+});
+
+figma.connect(ChevronLeftIcon, 'https://www.figma.com/design/Ni2hCq5zflPlamYJfpIV68/Laurelma-DS?node-id=14:2673', {
+  example: () => <ChevronLeftIcon />,
+});
+
+figma.connect(ChevronRightIcon, 'https://www.figma.com/design/Ni2hCq5zflPlamYJfpIV68/Laurelma-DS?node-id=14:2676', {
+  example: () => <ChevronRightIcon />,
+});
+
+figma.connect(MoreHorizontalIcon, 'https://www.figma.com/design/Ni2hCq5zflPlamYJfpIV68/Laurelma-DS?node-id=14:2679', {
+  example: () => <MoreHorizontalIcon />,
+});
+
+figma.connect(MoreVerticalIcon, 'https://www.figma.com/design/Ni2hCq5zflPlamYJfpIV68/Laurelma-DS?node-id=14:2684', {
+  example: () => <MoreVerticalIcon />,
+});
+
+// ── Actions ──────────────────────────────────────────────────────────
+
+figma.connect(SearchIcon, 'https://www.figma.com/design/Ni2hCq5zflPlamYJfpIV68/Laurelma-DS?node-id=14:2689', {
+  example: () => <SearchIcon />,
+});
+
+figma.connect(FilterIcon, 'https://www.figma.com/design/Ni2hCq5zflPlamYJfpIV68/Laurelma-DS?node-id=14:2693', {
+  example: () => <FilterIcon />,
+});
+
+figma.connect(SortIcon, 'https://www.figma.com/design/Ni2hCq5zflPlamYJfpIV68/Laurelma-DS?node-id=14:2696', {
+  example: () => <SortIcon />,
+});
+
+figma.connect(EditIcon, 'https://www.figma.com/design/Ni2hCq5zflPlamYJfpIV68/Laurelma-DS?node-id=14:2702', {
+  example: () => <EditIcon />,
+});
+
+figma.connect(TrashIcon, 'https://www.figma.com/design/Ni2hCq5zflPlamYJfpIV68/Laurelma-DS?node-id=14:2706', {
+  example: () => <TrashIcon />,
+});
+
+figma.connect(CopyIcon, 'https://www.figma.com/design/Ni2hCq5zflPlamYJfpIV68/Laurelma-DS?node-id=14:2713', {
+  example: () => <CopyIcon />,
+});
+
+figma.connect(DownloadIcon, 'https://www.figma.com/design/Ni2hCq5zflPlamYJfpIV68/Laurelma-DS?node-id=14:2717', {
+  example: () => <DownloadIcon />,
+});
+
+figma.connect(UploadIcon, 'https://www.figma.com/design/Ni2hCq5zflPlamYJfpIV68/Laurelma-DS?node-id=14:2722', {
+  example: () => <UploadIcon />,
+});
+
+figma.connect(ShareIcon, 'https://www.figma.com/design/Ni2hCq5zflPlamYJfpIV68/Laurelma-DS?node-id=14:2727', {
+  example: () => <ShareIcon />,
+});
+
+figma.connect(PlusIcon, 'https://www.figma.com/design/Ni2hCq5zflPlamYJfpIV68/Laurelma-DS?node-id=14:2734', {
+  example: () => <PlusIcon />,
+});
+
+figma.connect(MinusIcon, 'https://www.figma.com/design/Ni2hCq5zflPlamYJfpIV68/Laurelma-DS?node-id=14:2738', {
+  example: () => <MinusIcon />,
+});
+
+figma.connect(RefreshIcon, 'https://www.figma.com/design/Ni2hCq5zflPlamYJfpIV68/Laurelma-DS?node-id=14:2741', {
+  example: () => <RefreshIcon />,
+});
+
+figma.connect(SaveIcon, 'https://www.figma.com/design/Ni2hCq5zflPlamYJfpIV68/Laurelma-DS?node-id=14:2745', {
+  example: () => <SaveIcon />,
+});
+
+figma.connect(ClipboardIcon, 'https://www.figma.com/design/Ni2hCq5zflPlamYJfpIV68/Laurelma-DS?node-id=14:2750', {
+  example: () => <ClipboardIcon />,
+});
+
+// ── Status ───────────────────────────────────────────────────────────
+
+figma.connect(CheckIcon, 'https://www.figma.com/design/Ni2hCq5zflPlamYJfpIV68/Laurelma-DS?node-id=14:2754', {
+  example: () => <CheckIcon />,
+});
+
+figma.connect(XIcon, 'https://www.figma.com/design/Ni2hCq5zflPlamYJfpIV68/Laurelma-DS?node-id=14:2757', {
+  example: () => <XIcon />,
+});
+
+figma.connect(InfoIcon, 'https://www.figma.com/design/Ni2hCq5zflPlamYJfpIV68/Laurelma-DS?node-id=14:2761', {
+  example: () => <InfoIcon />,
+});
+
+figma.connect(WarningIcon, 'https://www.figma.com/design/Ni2hCq5zflPlamYJfpIV68/Laurelma-DS?node-id=14:2766', {
+  example: () => <WarningIcon />,
+});
+
+figma.connect(AlertCircleIcon, 'https://www.figma.com/design/Ni2hCq5zflPlamYJfpIV68/Laurelma-DS?node-id=14:2771', {
+  example: () => <AlertCircleIcon />,
+});
+
+figma.connect(CheckCircleIcon, 'https://www.figma.com/design/Ni2hCq5zflPlamYJfpIV68/Laurelma-DS?node-id=14:2776', {
+  example: () => <CheckCircleIcon />,
+});
+
+figma.connect(XCircleIcon, 'https://www.figma.com/design/Ni2hCq5zflPlamYJfpIV68/Laurelma-DS?node-id=14:2780', {
+  example: () => <XCircleIcon />,
+});
+
+figma.connect(HelpCircleIcon, 'https://www.figma.com/design/Ni2hCq5zflPlamYJfpIV68/Laurelma-DS?node-id=14:2785', {
+  example: () => <HelpCircleIcon />,
+});
+
+figma.connect(BanIcon, 'https://www.figma.com/design/Ni2hCq5zflPlamYJfpIV68/Laurelma-DS?node-id=14:2790', {
+  example: () => <BanIcon />,
+});
+
+// ── Social ───────────────────────────────────────────────────────────
+
+figma.connect(HeartIcon, 'https://www.figma.com/design/Ni2hCq5zflPlamYJfpIV68/Laurelma-DS?node-id=14:2794', {
+  example: () => <HeartIcon />,
+});
+
+figma.connect(StarIcon, 'https://www.figma.com/design/Ni2hCq5zflPlamYJfpIV68/Laurelma-DS?node-id=14:2797', {
+  example: () => <StarIcon />,
+});
+
+figma.connect(BookmarkIcon, 'https://www.figma.com/design/Ni2hCq5zflPlamYJfpIV68/Laurelma-DS?node-id=14:2800', {
+  example: () => <BookmarkIcon />,
+});
+
+figma.connect(BellIcon, 'https://www.figma.com/design/Ni2hCq5zflPlamYJfpIV68/Laurelma-DS?node-id=14:2803', {
+  example: () => <BellIcon />,
+});
+
+figma.connect(ThumbsUpIcon, 'https://www.figma.com/design/Ni2hCq5zflPlamYJfpIV68/Laurelma-DS?node-id=14:2807', {
+  example: () => <ThumbsUpIcon />,
+});
+
+// ── Media ────────────────────────────────────────────────────────────
+
+figma.connect(PlayIcon, 'https://www.figma.com/design/Ni2hCq5zflPlamYJfpIV68/Laurelma-DS?node-id=14:2811', {
+  example: () => <PlayIcon />,
+});
+
+figma.connect(PauseIcon, 'https://www.figma.com/design/Ni2hCq5zflPlamYJfpIV68/Laurelma-DS?node-id=14:2814', {
+  example: () => <PauseIcon />,
+});
+
+figma.connect(ImageIcon, 'https://www.figma.com/design/Ni2hCq5zflPlamYJfpIV68/Laurelma-DS?node-id=14:2818', {
+  example: () => <ImageIcon />,
+});
+
+figma.connect(CameraIcon, 'https://www.figma.com/design/Ni2hCq5zflPlamYJfpIV68/Laurelma-DS?node-id=14:2823', {
+  example: () => <CameraIcon />,
+});
+
+figma.connect(MusicIcon, 'https://www.figma.com/design/Ni2hCq5zflPlamYJfpIV68/Laurelma-DS?node-id=14:2827', {
+  example: () => <MusicIcon />,
+});
+
+// ── UI ───────────────────────────────────────────────────────────────
+
+figma.connect(HomeIcon, 'https://www.figma.com/design/Ni2hCq5zflPlamYJfpIV68/Laurelma-DS?node-id=14:2832', {
+  example: () => <HomeIcon />,
+});
+
+figma.connect(UserIcon, 'https://www.figma.com/design/Ni2hCq5zflPlamYJfpIV68/Laurelma-DS?node-id=14:2836', {
+  example: () => <UserIcon />,
+});
+
+figma.connect(UsersIcon, 'https://www.figma.com/design/Ni2hCq5zflPlamYJfpIV68/Laurelma-DS?node-id=14:2840', {
+  example: () => <UsersIcon />,
+});
+
+figma.connect(SettingsIcon, 'https://www.figma.com/design/Ni2hCq5zflPlamYJfpIV68/Laurelma-DS?node-id=14:2846', {
+  example: () => <SettingsIcon />,
+});
+
+figma.connect(MenuIcon, 'https://www.figma.com/design/Ni2hCq5zflPlamYJfpIV68/Laurelma-DS?node-id=14:2850', {
+  example: () => <MenuIcon />,
+});
+
+figma.connect(EyeIcon, 'https://www.figma.com/design/Ni2hCq5zflPlamYJfpIV68/Laurelma-DS?node-id=14:2855', {
+  example: () => <EyeIcon />,
+});
+
+figma.connect(EyeOffIcon, 'https://www.figma.com/design/Ni2hCq5zflPlamYJfpIV68/Laurelma-DS?node-id=14:2859', {
+  example: () => <EyeOffIcon />,
+});
+
+figma.connect(LockIcon, 'https://www.figma.com/design/Ni2hCq5zflPlamYJfpIV68/Laurelma-DS?node-id=14:2865', {
+  example: () => <LockIcon />,
+});
+
+figma.connect(CalendarIcon, 'https://www.figma.com/design/Ni2hCq5zflPlamYJfpIV68/Laurelma-DS?node-id=14:2869', {
+  example: () => <CalendarIcon />,
+});
+
+figma.connect(MailIcon, 'https://www.figma.com/design/Ni2hCq5zflPlamYJfpIV68/Laurelma-DS?node-id=14:2875', {
+  example: () => <MailIcon />,
+});
+
+figma.connect(ClockIcon, 'https://www.figma.com/design/Ni2hCq5zflPlamYJfpIV68/Laurelma-DS?node-id=14:2879', {
+  example: () => <ClockIcon />,
+});
+
+figma.connect(GlobeIcon, 'https://www.figma.com/design/Ni2hCq5zflPlamYJfpIV68/Laurelma-DS?node-id=14:2883', {
+  example: () => <GlobeIcon />,
+});
+
+figma.connect(LinkIcon, 'https://www.figma.com/design/Ni2hCq5zflPlamYJfpIV68/Laurelma-DS?node-id=14:2888', {
+  example: () => <LinkIcon />,
+});
+
+figma.connect(ExternalLinkIcon, 'https://www.figma.com/design/Ni2hCq5zflPlamYJfpIV68/Laurelma-DS?node-id=14:2892', {
+  example: () => <ExternalLinkIcon />,
+});
+
+figma.connect(CodeIcon, 'https://www.figma.com/design/Ni2hCq5zflPlamYJfpIV68/Laurelma-DS?node-id=14:2897', {
+  example: () => <CodeIcon />,
+});
+
+figma.connect(TerminalIcon, 'https://www.figma.com/design/Ni2hCq5zflPlamYJfpIV68/Laurelma-DS?node-id=14:2901', {
+  example: () => <TerminalIcon />,
+});
+
+figma.connect(DatabaseIcon, 'https://www.figma.com/design/Ni2hCq5zflPlamYJfpIV68/Laurelma-DS?node-id=14:2905', {
+  example: () => <DatabaseIcon />,
+});
+
+figma.connect(MapPinIcon, 'https://www.figma.com/design/Ni2hCq5zflPlamYJfpIV68/Laurelma-DS?node-id=14:2910', {
+  example: () => <MapPinIcon />,
+});
+
+figma.connect(LayersIcon, 'https://www.figma.com/design/Ni2hCq5zflPlamYJfpIV68/Laurelma-DS?node-id=14:2914', {
+  example: () => <LayersIcon />,
+});
+
+figma.connect(ZapIcon, 'https://www.figma.com/design/Ni2hCq5zflPlamYJfpIV68/Laurelma-DS?node-id=14:2919', {
+  example: () => <ZapIcon />,
+});
